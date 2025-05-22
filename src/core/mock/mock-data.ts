bd3f9640b10f159bdf8693ccd8f8b1e76d3148a7
tianp1n <pinedchr1@gmail.com>
2025-05-22 13:55:59 -0700

    added book interface + mock data on a new folder "core"

---
+++ src/core/mock/mock-data.ts
@@ -0,0 +1,49 @@
+import {IBooks} from "core/model/books-model";
+
+const books: IBooks[] = [
+    {
+        book_id: 1,
+        isbn13: 1212121212121,
+        original_publication_year: 2001,
+        original_title: "Arctic Action OG",
+        title: "Arctic Action",
+        image_url: "www.image.com",
+        small_image_url: "www.small-image.com"
+    },
+    {
+        book_id: 2,
+        isbn13: 3434343434343,
+        original_publication_year: 2002,
+        original_title: "Banana Bonanza OG",
+        title: "Banana Bonanza",
+        image_url: "www.image.com",
+        small_image_url: "www.small-image.com"
+    },
+    {
+        book_id: 3,
+        isbn13: 5656565656565,
+        original_publication_year: 2003,
+        original_title: "Code and Creativity OG",
+        title: "Code and Creativity",
+        image_url: "www.image.com",
+        small_image_url: "www.small-image.com"
+    },
+    {
+        book_id: 4,
+        isbn13: 7878787878787,
+        original_publication_year: 2004,
+        original_title: "Dear Dad OG",
+        title: "Dear Dad",
+        image_url: "www.image.com",
+        small_image_url: "www.small-image.com"
+    },
+    {
+        book_id: 5,
+        isbn13: 9090909090909,
+        original_publication_year: 2005,
+        original_title: "Ending Excerpt OG",
+        title: "Ending Excerpt",
+        image_url: "www.image.com",
+        small_image_url: "www.small-image.com"
+    }
+]
---
+++ src/core/model/books-model.ts
@@ -0,0 +1,9 @@
+export interface IBooks {
+    book_id: number;
+    isbn13: number;
+    original_publication_year: number;
+    original_title: string;
+    title: string;
+    image_url: string;
+    small_image_url: string;
+}
