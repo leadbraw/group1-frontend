1d8481cd8a4cf645ea1f60e911a7d1af417abf56
Noah <noah.dangerfield@gmail.com>
2025-05-22 21:29:22 -0700

    whoops I didnt understand layout.tsx properly

--- src/app/(auth)/reset-pass/page.tsx
+++ src/app/(auth)/reset-pass/page.tsx
@@ -1,12 +1,7 @@
-import AuthGuard from 'utils/route-guard/AuthGuard';
 import ResetPassword from 'views/auth/reset-password';
 
 // ==============================|| PAGE ||============================== //
 
 export default function ResetPassPage() {
-  return (
-    <AuthGuard>
-      <ResetPassword />
-    </AuthGuard>
-  );
+  return <ResetPassword />;
 }
