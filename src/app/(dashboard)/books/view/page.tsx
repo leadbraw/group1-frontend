800059f53fcf02551f711b8026d2cbb8f70b2464
Gage Hanby <gdhanby@uw.edu>
2025-05-27 13:47:00 -0700

    Potential fix for page view not working

--- src/app/(dashboard)/books/view/page.tsx
+++ src/app/(dashboard)/books/view/page.tsx
@@ -1,7 +1,8 @@
 import BookItem from 'views/books/books-view';
-
+import { getIsbn } from 'components/BookListItem';
 // ==============================|| PAGE ||============================== //
 
 export default function BookItemViewPge() {
-  return <BookItem isbn13={1212121212121}/>;
+  const isbn = getIsbn();
+  return <BookItem isbn13={isbn}/>;
 }
