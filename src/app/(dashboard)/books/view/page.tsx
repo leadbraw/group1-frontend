450ca464aa953a3ed493b82511996a40acc76cf9
tianp1n <pinedchr1@gmail.com>
2025-05-31 17:34:27 -0700

    Fixed more mock data stuff

--- src/app/(dashboard)/books/view/page.tsx
+++ src/app/(dashboard)/books/view/page.tsx
@@ -2,13 +2,24 @@ import { BookItem } from 'components/BookItem';
 
 export default function BookItemViewPge() {
   const book = {
-    book_id: 1,
+    authors: 'Archie Adams',
+    icons: {
+      large: 'https://images.gr-assets.com/books/1361039443m/41865.jpg',
+      small: 'https://images.gr-assets.com/books/1361039443s/41865.jpg'
+    },
     isbn13: 1212121212121,
-    original_publication_year: 2001,
     original_title: 'Arctic Action OG',
-    title: 'Arctic Action',
-    image_url: 'https://images.gr-assets.com/books/1361039443m/41865.jpg',
-    small_image_url: 'https://images.gr-assets.com/books/1361039443s/41865.jpg'
+    publication: 2001,
+    ratings: {
+      average: 3.1,
+      count: 10001,
+      rating_1: 2000,
+      rating_2: 2000,
+      rating_3: 2000,
+      rating_4: 2000,
+      rating_5: 2001
+    },
+    title: 'Arctic Action'
   };
   return <BookItem book={book} />;
 }
