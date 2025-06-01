a7eaf77231c28da7e9fe73120f44420214a6dc83
tianp1n <pinedchr1@gmail.com>
2025-05-31 18:08:01 -0700

    trying to use books-list on authorsearch

--- src/views/books/search-by-author.tsx
+++ src/views/books/search-by-author.tsx
@@ -3,7 +3,14 @@
 import React, { useState } from 'react';
 
 // material-ui
+import Avatar from '@mui/material/Avatar';
+import BookIcon from '@mui/icons-material/Book';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
@@ -16,7 +23,9 @@ import * as Yup from 'yup';
 // project import
 import axios from 'utils/axios';
 import { IBook } from 'types/books';
-import { BookListItem } from 'components/BookListItem';
+import { BookListItem, NoBook } from 'components/BookListItem';
+
+const defaultTheme = createTheme();
 
 const AuthorSearch = () => {
   const [books, setBooks] = useState<IBook[]>([]);
@@ -63,11 +72,41 @@ const AuthorSearch = () => {
             {error && <Typography color="error">{error}</Typography>}
 
             {books.length > 0 && (
-              <List>
-                {books.map((book) => (
-                  <BookListItem key={book.isbn13} book={book} />
-                ))}
-              </List>
+              <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="md">
+                  <CssBaseline />
+                  <Box
+                    sx={{
+                      marginTop: 8,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center'
+                    }}
+                  >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                      <BookIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                      Book List
+                    </Typography>
+
+                    <Box sx={{ mt: 4, width: '100%' }}>
+                      <List>
+                        {books.length > 0 ? (
+                          books.map((book, index: number) => (
+                            <React.Fragment key={book.isbn13}>
+                              <BookListItem book={book} />
+                              {index < books.length - 1 && <Divider component="li" />}
+                            </React.Fragment>
+                          ))
+                        ) : (
+                          <NoBook />
+                        )}
+                      </List>
+                    </Box>
+                  </Box>
+                </Container>
+              </ThemeProvider>
             )}
           </Stack>
         </form>
@@ -75,5 +114,11 @@ const AuthorSearch = () => {
     </Formik>
   );
 };
+
+// <List>
+//   {books.map((book) => (
+//     <BookListItem key={book.isbn13} book={book} />
+//   ))}
+// </List>
 
 export default AuthorSearch;
