89ed41d05bea4c62aedf379b74ff6c51206b9f0f
Noah <noah.dangerfield@gmail.com>
2025-06-06 18:55:06 -0700

    fix duplicate footer appearing in create new book view

--- src/views/books/books-new.tsx
+++ src/views/books/books-new.tsx
@@ -11,18 +11,16 @@ import NewBookForm from 'sections/books/NewBookForm';
 
 export default function NewBook() {
   return (
-    <AuthWrapper>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Stack sx={{ mb: { xs: -0.5, sm: 0.5 } }} spacing={1}>
-            <Typography variant="h3">Create a New Book</Typography>
-            <Typography color="secondary">Fill out the form below to add a new book</Typography>
-          </Stack>
-        </Grid>
-        <Grid item xs={12}>
-          <NewBookForm />
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Stack sx={{ mb: { xs: -0.5, sm: 0.5 } }} spacing={1}>
+          <Typography variant="h3">Create a New Book</Typography>
+          <Typography color="secondary">Fill out the form below to add a new book</Typography>
+        </Stack>
       </Grid>
-    </AuthWrapper>
+      <Grid item xs={12}>
+        <NewBookForm />
+      </Grid>
+    </Grid>
   );
 }
