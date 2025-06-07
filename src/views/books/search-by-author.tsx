97f203866b535e010c3f31f3960bb0ec8d1e3042
Noah <noah.dangerfield@gmail.com>
2025-06-06 19:18:59 -0700

    surely this works

--- src/views/books/search-by-author.tsx
+++ src/views/books/search-by-author.tsx
@@ -41,7 +41,6 @@ const AuthorSearch = () => {
         try {
           const res = await axios.get(`/books/author/${encodeURIComponent(values.author)}`);
           setBooks(res.data.books);
-          console.log('The books:', res.data); // DEBUG LINE
         } catch (err: any) {
           setError(err.message || 'Failed to fetch books.');
         } finally {
@@ -50,73 +49,73 @@ const AuthorSearch = () => {
       }}
     >
       {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-        <form onSubmit={handleSubmit} noValidate>
-          <Stack spacing={2}>
-            <Typography variant="h6">Author</Typography>
-            <TextField
-              name="author"
-              fullWidth
-              value={values.author}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={touched.author && Boolean(errors.author)}
-              helperText={touched.author && errors.author}
-              placeholder="Enter name"
-            />
-            <Button type="submit" variant="contained" disabled={isSubmitting}>
-              Search
-            </Button>
+        <>
+          <form onSubmit={handleSubmit} noValidate>
+            <Stack spacing={2}>
+              <Typography variant="h6">Author</Typography>
+              <TextField
+                name="author"
+                fullWidth
+                value={values.author}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.author && Boolean(errors.author)}
+                helperText={touched.author && errors.author}
+                placeholder="Enter name"
+              />
+              <Button type="submit" variant="contained" disabled={isSubmitting}>
+                Search
+              </Button>
+              {error && <Typography color="error">{error}</Typography>}
+            </Stack>
+          </form>
 
-            {error && <Typography color="error">{error}</Typography>}
-
-            <ThemeProvider theme={defaultTheme}>
-              <Container component="main" maxWidth="md">
-                <CssBaseline />
+          <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="md">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <Typography component="h1" variant="h5">
+                  Results
+                </Typography>
                 <Box
                   sx={{
-                    marginTop: 8,
+                    mt: 4,
+                    width: '100%',
                     display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
+                    alignItems: 'center'
                   }}
                 >
-                  <Typography component="h1" variant="h5">
-                    Results
-                  </Typography>
-
-                  <Box
-                    sx={{
-                      mt: 4,
-                      width: '100%',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                    }}
-                  >
-                    <List sx={{ width: '100%', maxWidth: 600 }}>
-                      {books.length > 0 ? (
-                        books.map((book, index: number) => (
-                          <React.Fragment key={book.isbn13}>
-                            <BookListItem
-                              book={book}
-                              onDelete={function (isbn13: number): void {
-                                throw new Error('Function not implemented.');
-                              }}
-                            />
-                            {index < books.length - 1 && <Divider component="li" />}
-                          </React.Fragment>
-                        ))
-                      ) : (
-                        <NoBook />
-                      )}
-                    </List>
-                  </Box>
+                  <List sx={{ width: '100%', maxWidth: 600 }}>
+                    {books.length > 0 ? (
+                      books.map((book, index: number) => (
+                        <React.Fragment key={book.isbn13}>
+                          <BookListItem
+                            book={book}
+                            onDelete={function (isbn13: number): void {
+                              throw new Error('Function not implemented.');
+                            }}
+                          />
+                          {index < books.length - 1 && <Divider component="li" />}
+                        </React.Fragment>
+                      ))
+                    ) : (
+                      <NoBook />
+                    )}
+                  </List>
                 </Box>
-              </Container>
-            </ThemeProvider>
-          </Stack>
-        </form>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </>
       )}
     </Formik>
   );
