3338cb450315a22c1a71a65dd7e09306db7d86ff
Noah <noah.dangerfield@gmail.com>
2025-06-06 19:08:56 -0700

    hacky fix for new book form formatting

--- src/views/books/books-new.tsx
+++ src/views/books/books-new.tsx
@@ -1,7 +1,9 @@
 // material-ui
+import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import AuthCard from 'sections/auth/AuthCard';
 
 // project import
 import NewBookForm from 'sections/books/NewBookForm';
@@ -18,7 +20,26 @@ export default function NewBook() {
         </Stack>
       </Grid>
       <Grid item xs={12}>
-        <NewBookForm />
+        <Box sx={{ minHeight: '100vh' }}>
+          <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+            <Grid item xs={12}>
+              <Grid
+                item
+                xs={12}
+                container
+                justifyContent="center"
+                alignItems="center"
+                sx={{ minHeight: { xs: 'calc(100vh - 210px)', sm: 'calc(100vh - 134px)', md: 'calc(100vh - 112px)' } }}
+              >
+                <Grid item>
+                  <AuthCard>
+                    <NewBookForm />
+                  </AuthCard>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </Grid>
     </Grid>
   );
