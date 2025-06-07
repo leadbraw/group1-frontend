41902293294377f3284ec8cbe387db4a7589d4af
Noah <noah.dangerfield@gmail.com>
2025-06-06 19:36:16 -0700

    search by year formatting fix

--- src/views/books/search-by-year.tsx
+++ src/views/books/search-by-year.tsx
@@ -53,64 +53,74 @@ const YearSearch = () => {
       }}
     >
       {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-        <form onSubmit={handleSubmit} noValidate>
-          <Stack spacing={2}>
-            <Typography variant="h6">Year</Typography>
-            <TextField
-              name="year"
-              fullWidth
-              value={values.year}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={touched.year && Boolean(errors.year)}
-              helperText={touched.year && errors.year}
-              placeholder="Enter year of publication"
-            />
-            <Button type="submit" variant="contained" disabled={isSubmitting}>
-              Search
-            </Button>
+        <>
+          <form onSubmit={handleSubmit} noValidate>
+            <Stack spacing={2}>
+              <Typography variant="h6">Year</Typography>
+              <TextField
+                name="year"
+                fullWidth
+                value={values.year}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.year && Boolean(errors.year)}
+                helperText={touched.year && errors.year}
+                placeholder="Enter year of publication"
+              />
+              <Button type="submit" variant="contained" disabled={isSubmitting}>
+                Search
+              </Button>
 
-            {error && <Typography color="error">{error}</Typography>}
+              {error && <Typography color="error">{error}</Typography>}
+            </Stack>
+          </form>
+          <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="md">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }}
+              >
+                <Typography component="h1" variant="h5">
+                  Results
+                </Typography>
 
-            <ThemeProvider theme={defaultTheme}>
-              <Container component="main" maxWidth="md">
-                <CssBaseline />
                 <Box
                   sx={{
-                    marginTop: 8,
+                    mt: 4,
+                    width: '100%',
                     display: 'flex',
-                    flexDirection: 'column',
+                    justifyContent: 'center',
                     alignItems: 'center'
                   }}
                 >
-                  <Typography component="h1" variant="h5">
-                    Results
-                  </Typography>
-
-                  <Box sx={{ mt: 4, width: '100%' }}>
-                    <List>
-                      {books.length > 0 ? (
-                        books.map((book, index: number) => (
-                          <React.Fragment key={book.isbn13}>
-                            <BookListItem
-                              book={book}
-                              onDelete={function (isbn13: number): void {
-                                throw new Error('Function not implemented.');
-                              }}
-                            />
-                            {index < books.length - 1 && <Divider component="li" />}
-                          </React.Fragment>
-                        ))
-                      ) : (
-                        <NoBook />
-                      )}
-                    </List>
-                  </Box>
+                  <List sx={{ width: '100%', maxWidth: 800 }}>
+                    {books.length > 0 ? (
+                      books.map((book, index: number) => (
+                        <React.Fragment key={book.isbn13}>
+                          <BookListItem
+                            book={book}
+                            onDelete={function (isbn13: number): void {
+                              throw new Error('Function not implemented.');
+                            }}
+                          />
+                          {index < books.length - 1 && <Divider component="li" />}
+                        </React.Fragment>
+                      ))
+                    ) : (
+                      <NoBook />
+                    )}
+                  </List>
                 </Box>
-              </Container>
-            </ThemeProvider>
-          </Stack>
-        </form>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </>
       )}
     </Formik>
   );
