5c0d71bd3590987a10af56e6e1d9eaa1fec709ec
Noah <noah.dangerfield@gmail.com>
2025-06-06 18:48:21 -0700

    hjjhgghj

--- src/views/books/books-view.tsx
+++ src/views/books/books-view.tsx
@@ -1,17 +1,14 @@
 'use client';
-import books from "core/mock/mock-data";
-import { IBook } from "types/books";
-import { BookItem } from "components/BookItem";
-import notfound from "app/not-found";
+import books from 'core/mock/mock-data';
+import { IBook } from 'types/books';
+import { BookItem } from 'components/BookItem';
+import notfound from 'app/not-found';
 
-export default function BookView({ isbn13 }: {isbn13: number}) {
-    const book: IBook | undefined = books.find(
-    (b) => b.isbn13 === isbn13
-  );
+export default function BookView({ isbn13 }: { isbn13: number }) {
+  const book: IBook | undefined = books.find((b) => b.isbn13 === isbn13);
   if (book) {
     return <BookItem book={book as IBook} />;
   } else {
     notfound();
   }
-  
 }
